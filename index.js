390bdf0b1c767c0bdeb916c6468b4aabe9ace6e8
pajith551@gmail.com <pajith551@gmail.com>
2022-02-28 22:14:53 +0530

    audui play

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for(var i = 0; i<numberOfDrumButtons; i++){
-    document.querySelectorAll("button")[i].addEventListener("click", handleClick);
+    document.querySelectorAll("button")[i].addEventListener("click", function(){
 
-    function handleClick(){
-        alert(" i am clicked");
-    }
+    var audio = new Audio('sounds/tom-1.mp3');
+audio.play();
+    
+    })
+
 }
+
+
+// var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+// for(var i = 0; i<numberOfDrumButtons; i++){
+//     document.querySelectorAll("button")[i].addEventListener("click", handleClick);
+
+//     function handleClick(){
+//         alert(" i am clicked");
+//     }
+// }
 
 
 // this is same as above code but in Anonymous function
 // document.querySelector("button").addEventListener("click", function(){
 //     alert(" i am clicked");
 // });
+
+
 
